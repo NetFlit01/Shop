95d89478da32ee2ffbf7357a8a5e5a31388869a4
Nikita <nikita-dock@list.ru>
2024-02-15 22:45:29 +0300

    Вынесен в отдельный файл скрипт проверки введенных данных пользователем

--- js/script.js
+++ js/script.js
@@ -28,29 +28,4 @@ function handleClick(button) {
 }
 
 
-//Скрипт для проверки заполнения форм данными
-function validateForm(event) {
-    var fullName = document.getElementById("full-name").value;
-    var deliveryAddress = document.getElementById("delivery-address").value;
-    var email = document.getElementById("email").value;
-
-    if (fullName == "" || deliveryAddress == "" || email == "") {
-        if (fullName == "") {
-            alert("Пожалуйста, введите ФИО");
-            return false
-        }
-        if (deliveryAddress == "") {
-            alert("Пожалуйста, введите адрес доставки");
-            return false
-        }
-        if (email == "") {
-            alert("Пожалуйста, введите адрес электронной почты");
-            return false
-        }
-    } else {
-        alert("Ваш заказ оформлен. Спасибо, что обратились в наш магазин!");
-        return true
-    }
-}
-
 
